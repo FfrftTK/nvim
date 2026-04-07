267b352ae40ba777778fa4b8e72805693e5bd4a1
FfrftTK <fr5tdkr20@gmail.com>
2026-04-07 14:42:48 +0900

    fix: disable win32yank on mac

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.clipboard = "unnamedplus"
-if vim.fn.has("wsl") then
+if vim.fn.has("wsl") and not vim.fn.has("mac") then
   vim.g.clipboard = {
     name = "win32yank-wsl",
     copy = {
