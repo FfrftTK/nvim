6f0da10e0b3ce669e521b25969e1284f42c80dd7
FfrftTK <fr5tdkr20@gmail.com>
2026-04-07 14:57:57 +0900

    disable im-select

--- lua/plugins/im-select.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "keaising/im-select.nvim",
-  event = "InsertEnter",
-  opts = {
-    default_im_select = "com.apple.keylayout.ABC",
-    default_command = "im-select",
-    set_previous_events = {},
-  },
-}
